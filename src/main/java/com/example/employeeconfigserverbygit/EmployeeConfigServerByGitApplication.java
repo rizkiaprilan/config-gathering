4f79e9d3873ef40db3a0b499b99525c605c9c837
Rizki Aprilan <riskiazza@gmail.com>
2022-03-18 14:07:04 +0700

    initial

---
+++ src/main/java/com/example/employeeconfigserverbygit/EmployeeConfigServerByGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeeconfigserverbygit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeConfigServerByGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeConfigServerByGitApplication.class, args);
+	}
+
+}
